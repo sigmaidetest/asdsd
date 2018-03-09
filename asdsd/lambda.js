fe9af1ffaf8309d3c6eb5db285fc1c1ca5561389
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-03-09 11:32:47 +0530

    asasasas

---
+++ asdsd/lambda.js
@@ -0,0 +1,28 @@
+let AWS = require('aws-sdk');
+const ses = new AWS.SES();
+exports.handler = function (event, context, callback) {
+
+	ses.sendEmail({
+		Destination: {
+			CcAddresses: [''],
+			ToAddresses: ['randika@adroitlogic.com']
+		},
+		Message: {
+			Body: {
+				Text: {
+					Data: 'sample ses text'
+				}
+			},
+			Subject: {
+				Data: 'sample ses text'
+			}
+		},
+		Source: 'randika@adroitlogic.com',
+	}, function (err, data) {
+		if (err) console.log(err, err.stack); // an error occurred
+		else console.log(data);           // successful response
+	});
+
+
+	callback(null, 'Successfully executed');
+}
